b1651c9f37fe2f7b97e505a101b6848e7db40972
Desi <desi.devaul@gmail.com>
2021-01-21 22:21:51 -0500

    Extended the initialize method to add event listeners to each project div to bring up the display the corresponding tasks. Also, called the loadItems method on the first project in the projects array to load as a default.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import{ProjectItem} from  './projectItem'
 const todoList = (function(){
     const todoContainer = document.querySelector('#todos-container')
     const projectContainer = document.querySelector('#projects-container')
-    const projects = [new ProjectItem(), new ProjectItem(), new ProjectItem()]
+    const projects = [new ProjectItem(todoContainer), new ProjectItem(todoContainer), new ProjectItem(todoContainer)]
     function initialize() {
-        projects.forEach(project => projectContainer.appendChild(project.node))
-        projects[0].items.forEach(item => todoContainer.appendChild(item.node))
-        projects[0].node.classList.add('project-item-selected')
+        projects.forEach(project => {
+            projectContainer.appendChild(project.node)
+            project.node.addEventListener('click', () => project.loadItems(todoContainer, projects))
+        })
+        projects[0].loadItems(todoContainer, projects)
     }
 
     return {initialize}
