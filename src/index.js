e57b4626078540c992c15c209c1e34520c8147b2
Desi <desi.devaul@gmail.com>
2021-01-23 11:41:21 -0500

    Made this the de facto controller.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,83 @@
+import {AddToDoItem} from './addToDoItem'
+import {todoList} from './todoList'
 import {ToDoItem} from './todoItem'
-import{ProjectItem} from  './projectItem'
+import {ProjectItem} from './projectItem'
 
-const todoList = (function(){
-    const todoContainer = document.querySelector('#todos-container')
-    const projectContainer = document.querySelector('#projects-container')
-    const projects = [new ProjectItem(todoContainer), new ProjectItem(todoContainer), new ProjectItem(todoContainer)]
-    function initialize() {
-        projects.forEach(project => {
+
+let selectedProject 
+const todoContainer = todoList.todoContainer
+const projectContainer = todoList.projectContainer
+let projects = todoList.projects
+const addItemHeaderButton = document.querySelector('#todos-add-button')
+addItemHeaderButton.addEventListener('click', addItem)
+const addProjectButton = document.querySelector('#projects-add-button')
+addProjectButton.addEventListener('click', addProject)
+
+function initialize() {
+    projects.forEach(project => {
             projectContainer.appendChild(project.node)
-            project.node.addEventListener('click', () => project.loadItems(todoContainer, projects))
-        })
-        projects[0].loadItems(todoContainer, projects)
+            project.node.addEventListener('click', () => selectProject(project))
+            project.deleteButton.addEventListener('click', () => deleteProject(project))
+    })
+    selectProject(projects[0])
+}
+
+function selectProject(project){
+    if (selectedProject){
+        selectedProject.node.classList.remove('project-item-selected')
     }
+    selectedProject = project
+    selectedProject.node.classList.add('project-item-selected')            
+    loadItems()
+}
 
-    return {initialize}
-})()
 
+function loadItems(){
+    while (todoContainer.hasChildNodes()){
+        todoContainer.removeChild(todoContainer.firstChild)
+    }
+    selectedProject.items.forEach(item => {
+        todoContainer.appendChild(item.node)
+        item.delButton.addEventListener('click', () => deleteItem(item))
+    })
+    const add = new AddToDoItem()
+    todoContainer.appendChild(add.node)
+    add.node.addEventListener('click', addItem)
 
-todoList.initialize()
+    
+}
+
+function addItem(){
+    selectedProject.items.push(new ToDoItem())
+    todoContainer.removeChild(todoContainer.lastChild)
+    loadItems()
+}
+
+function deleteItem(item){
+    selectedProject.items = selectedProject.items.filter(i => {
+        return i !== item
+    })
+    loadItems()
+}
+
+function addProject(){
+    const newProj = new ProjectItem()
+    projects.push(newProj)
+    initialize()
+}
+
+function deleteProject(project){
+    projects = projects.filter(p => {
+        return p !== project
+    })
+    projectContainer.removeChild(project.node)
+}
+
+function clearProjects(){
+    while (projectContainer.hasChildNodes){
+
+    }
+}
+
+initialize()
 
